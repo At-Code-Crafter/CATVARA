ce32853e216d538bad1764ce4b3404c2706b8493
Arshdeep Singh <arshdeepjhagra@gmail.com>
2026-01-09 23:14:30 +0400

    public directory changed

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
         commands: __DIR__ . '/../routes/console.php',
@@ -13,10 +13,15 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'company.selected' => \App\Http\Middleware\EnsureCompanySelected::class,
-            'company.access'   => \App\Http\Middleware\EnsureCompanyAccess::class,
-            'company.context'  => \App\Http\Middleware\SetCurrentCompanyFromRoute::class,
+            'company.access' => \App\Http\Middleware\EnsureCompanyAccess::class,
+            'company.context' => \App\Http\Middleware\SetCurrentCompanyFromRoute::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+
+$app->usePublicPath($app->basePath('public_html'));
+
+return $app;
