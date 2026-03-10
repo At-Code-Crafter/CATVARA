3facf395d3eb03eb7ffad65dba7ab4015196293b
Arshdeep Singh <arshdeepjhagra@gmail.com>
2026-03-10 11:34:39 +0400

    dashboard

--- resources/views/theme/adminlte/dashboard.blade.php
+++ resources/views/theme/adminlte/dashboard.blade.php
@@ -16,7 +16,7 @@
 
 @section('content')
 
-  @if ($brandIds = user_brand_ids())
+  @if (user_brand_ids()->isNotEmpty())
     <div class="d-flex align-items-center justify-content-center" style="min-height: 70vh;">
       <h1 class="display-3 text-secondary font-weight-bold">{{ active_company()->name ?? 'Dashboard' }}</h1>
     </div>
